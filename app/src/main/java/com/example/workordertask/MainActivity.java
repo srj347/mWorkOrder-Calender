035ba11e41a4cc795e519c2e95acc5c9fe32d00e
Suraj Verma <surajverma0347@gmail.com>
2023-02-27 15:57:44 +0530

    initial project setup done

---
+++ app/src/main/java/com/example/workordertask/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.workordertask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
